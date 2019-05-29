a637879782a4d7b8c6be35056b1d62e5305d00f1
RahulRupnoor <51168648+RahulRupnoor@users.noreply.github.com>
2019-05-29 21:08:04 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            char[] input1 = new char[10];
+            for (int i = 0; i < input1.Length; i++)
+            {
+             input1[i]= Convert.ToChar(Console.Read());
+             Console.Write(input1.Count());
+            }
+            
+            
+
+            Console.ReadKey();
+        }
+    }
+}
